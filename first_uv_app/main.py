792a95539e4f22eb4eeb7572ac7e8c66975cb308
felipesantos2 <santospinheiro627@gmail.com>
2026-03-21 17:50:25 -0300

    tests with ruff and ty on vscode and cli

--- first_uv_app/main.py
+++ first_uv_app/main.py
@@ -1,5 +1,37 @@
-def main():
-    print("Hello from first-uv-app!")
+import calendar
+import datetime
+import mailbox
+import sys
+import time
 
-if __name__ == "__main__":
-    main()
+from rich.console import Console
+
+console = Console()
+
+
+
+def main():
+	print('Hello from first-uv-app!')
+
+
+def my_func(a: int, b: int, c: str, d: list) -> None:
+
+	def inner():
+		pass
+
+	def out():
+		pass
+
+
+my_func(1, 2, 'pinheiro', [1, 2, 3, 'felipe'])
+
+if __name__ == '__main__':
+	main()
+
+
+class Main:
+	def __init__(self) -> None:
+		pass
+
+
+ei = Main()
